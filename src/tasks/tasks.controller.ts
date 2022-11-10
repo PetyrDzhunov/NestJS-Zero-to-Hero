fe877671ebf866972f070231184b85c488ce48ff
PetarDzhunov <petar.djunov@abv.bg>
2022-11-10 15:02:05 +0200

    create task service created

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
